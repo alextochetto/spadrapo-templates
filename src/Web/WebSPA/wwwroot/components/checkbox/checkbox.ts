5607a76b8dfc2b260b31b3557f60f95b60181c16
Alex Tochetto <atochetto@sysphera.com>
2020-09-29 13:22:36 -0300

    change component parameters

--- src/Web/WebSPA/wwwroot/components/checkbox/checkbox.ts
+++ src/Web/WebSPA/wwwroot/components/checkbox/checkbox.ts
@@ -1,4 +1,4 @@
-async function checkboxConstructor(el: HTMLElement, app: any): Promise<any> {
+async function checkboxConstructor(el: HTMLElement, app: DrapoApplication): Promise<any> {
     //Initialize
     const checkbox: Checkbox = new Checkbox(el, app);
     await checkbox.Initalize();
@@ -11,9 +11,13 @@ class Checkbox {
     private _app: any;
     //Properties
     //Constructors
-    constructor(el: HTMLElement, app: any) {
+    constructor(el: HTMLElement, app: DrapoApplication) {
         this._el = el;
         this._app = app;
+    }
+
+    get Application(): DrapoApplication {
+        return this._app;
     }
 
     public async Initalize(): Promise<void> {
